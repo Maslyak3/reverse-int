dee68b2ea97ab8d7a165440cf9d6c50d6254507d
Roman Masliak <maslyak3@gmail.com>
2024-08-01 11:56:18 +0300

    init: function Reverse coding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let stringNumber = Math.abs(n).toString();
+    let splitNumber = stringNumber.split('');
+    let reversedArray = splitNumber.reverse();
+    let result = reversedArray.join('');
+    return parseInt(result);
 }
+
